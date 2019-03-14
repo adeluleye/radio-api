5d47a65c259f44c5fafa61f228198bd16b24e390
adeluleye <adelu4real@gmail.com>
2019-03-14 23:33:09 +0100

    implement routes for app settings

--- startup/routes.js
+++ startup/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const radios = require('../routes/radios');
 const cities = require('../routes/cities');
 const themes = require('../routes/themes');
+const settings = require('../routes/settings');
 const error = require('../middleware/error');
 
 module.exports = function (app) {
@@ -9,6 +10,7 @@ module.exports = function (app) {
     app.use('/api/radios', radios);
     app.use('/api/cities', cities);
     app.use('/api/themes', themes);
+    app.use('/api/settings', settings);
     
     // Express Error Middleware function is passed after all the existing middleware functions
     app.use(error);
