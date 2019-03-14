0e6111724288dc0c0c291ba672bc3338c1683d49
adeluleye <adelu4real@gmail.com>
2019-03-14 01:01:11 +0100

    implement model for radio city

---
+++ models/city.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const citySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 255
+    }
+});
+
+const City = mongoose.model('City', citySchema);
+
+function validateCity(city) {
+    const schema = {
+      name: Joi.string().min(3).max(255).required()
+    };
+  
+    return Joi.validate(city, schema);
+}
+
+module.exports.City = City;
+module.exports.citySchema = citySchema;
+module.exports.validateCity = validateCity;
